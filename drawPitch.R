410ec2ff2cf705bedd1569229a17ef042fe197f3
jogall <joedgallagher@gmail.com>
2017-09-13 09:47:26 +0100

    Created drawPitch() function

---
+++ drawPitch.R
@@ -0,0 +1,62 @@
+require(ggplot2)
+require(ggforce)
+
+# drawPitch()
+#---------------------------------------------------------------------------
+# Draw football (soccer) pitch with user-defined pitch length and width using 
+# ggplot. Uses function `ggforce::geom_circle`.
+#----------------------------------------------------------------------------
+# length = length of pitch in metres (m)
+# width = width of pitch in metres (m)
+#----------------------------------------------------------------------------
+
+drawPitch <- function(length = 105, width = 68) {
+  ggplot() +
+    # pitch
+    geom_rect(aes(xmin = -4, xmax = length + 4, ymin = -4, ymax = width + 4), fill = "#008000") +
+    # outer lines
+    geom_rect(aes(xmin = 0, xmax = length, ymin = 0, ymax = width), fill = "#008000", col = "white", lwd = 1.2) +
+    # centre circle
+    geom_circle(aes(x0 = length / 2, y0 = width / 2, r = 9.15), fill = "#008000", col = "white", lwd = 1.2) +
+    # kick off spot
+    geom_circle(aes(x0 = length / 2, y0 = width / 2, r = 0.5), fill = "white", col = "white", lwd = 1.2) +
+    # halfway line
+    geom_segment(aes(x = length / 2, y = 0, xend = length / 2, yend = width), col = "white", lwd = 1.2) +
+    # penalty arcs
+    geom_circle(aes(x0 = 11, y0 = width / 2, r = 9.15), fill = "#008000", col = "white", lwd = 1.2) +
+    geom_circle(aes(x0 = length - 11, y0 = width / 2, r = 9.15), fill = "#008000", col = "white", lwd = 1.2) +
+    # penalty areas
+    geom_rect(aes(xmin = 0, xmax = 16.5, ymin = width / 2 - (40.3 / 2), ymax = width / 2 + (40.3 / 2)), fill = "#008000", col = "white", lwd = 1.2) +
+    geom_rect(aes(xmin = length - 16.5, xmax = length, ymin = width / 2 - (40.3 / 2), ymax = width / 2 + (40.3 / 2)), fill = "#008000", col = "white", lwd = 1.2) +
+    # penalty spots
+    geom_circle(aes(x0 = 11, y0 = width / 2, r = 0.5), fill = "white", col = "white", lwd = 1.2) +
+    geom_circle(aes(x0 = length - 11, y0 = width / 2, r = 0.5), fill = "white", col = "white", lwd = 1.2) +
+    # six yard boxes
+    geom_rect(aes(xmin = 0, xmax = 5.5, ymin = (width / 2) - 9.16, ymax = (width / 2) + 9.16), fill = "#008000", col = "white", lwd = 1.2) +
+    geom_rect(aes(xmin = length - 5.5, xmax = length, ymin = (width / 2) - 9.16, ymax = (width / 2) + 9.16), fill = "#008000", col = "white", lwd = 1.2) +
+    # goals
+    geom_rect(aes(xmin = -2, xmax = 0, ymin = (width / 2) - 3.66, ymax = (width / 2) + 3.66), fill = "#008000", col = "white", lwd = 1.2) +
+    geom_rect(aes(xmin = length, xmax = length + 2, ymin = (width / 2) - 3.66, ymax = (width / 2) + 3.66), fill = "#008000", col = "white", lwd = 1.2) +
+    coord_fixed() +
+    theme(rect = element_blank(), 
+          line = element_blank(),
+          text = element_blank())
+}
+
+# Example #1: Plot real player position
+#---------------------------------------
+# #get sample dataset used in publication http://home.ifi.uio.no/paalh/publications/files/mmsys2014-dataset.pdf
+#dat <- read.csv(url("http://home.ifi.uio.no/paalh/dataset/alfheim/2013-11-03/zxy/2013-11-03_tromso_stromsgodset_raw_first.csv"), head=F)
+# #subset first minute (60 * 20 FPS) for player #9
+#dat <- subset(dat, id == 9)[1:1200,]
+# #draw path on pitch
+#pitch <- drawPitch(length = 105, width = 68)
+#pitch + geom_path(data = dat, aes(X, Y), lwd = 2)
+
+# Example #2: Plot simulated correlated walk
+#--------------------------------------------
+# #simulate 1200 frames of x,y-coordinates using distributions fitted to real player position data ()
+#dat <- simRun(1200, rho = 0.98, xlim = c(0,105), ylim = c(0,68), stat_pc = 0.11) 
+# draw path on pitch
+#pitch <- drawPitch(length = 105, width = 68)
+#pitch + geom_path(data = dat, aes(x = X, y = Y), lwd = 2)
