270dadca72cfe5f73b484e4472ec70f4e810d3ca
jogall <joedgallagher@gmail.com>
2017-09-29 08:59:45 +0100

    Added dataset tromso_extra and updated documentation

---
+++ R/data-tromso_extra.R
@@ -0,0 +1,18 @@
+#' x,y-coordinates and additional information on 11 soccer players over 10 minutes (12000 frames each)
+#'
+#' Raw x,y data on movements of 11 soccer players (1'-10'; Tromsø IL vs. Anzhi, 2013-11-07), plus additional information on player heading, direction, energy, speed and total distance. Data captured at 20 Hz using the ZXY Sport Tracking system and made available in the publication \href{http://home.ifi.uio.no/paalh/dataset/alfheim/}{ZXY Sport Tracking}.
+#' @docType data
+#' @name data-tromso
+#' @format A dataframe containing 12000 frames of x,y-coordinates and timestamps from 11 players.
+#' @source \href{http://home.ifi.uio.no/paalh/dataset/alfheim/}{ZXY Sport Tracking}
+#' @references Pettersen et al. (2014) Proceedings of the International Conference on Multimedia Systems (MMSys)
+#' (\href{http://home.ifi.uio.no/paalh/publications/files/mmsys2014-dataset.pdf}{pdf})
+#' @usage data(tromso)
+#' @examples
+#' \dontrun{
+#' data(tromso)
+#' # draw path of player #8 on a soccer pitch
+#' soccerPitchBG(lengthPitch = 105, widthPitch = 68, grass = TRUE) + 
+#'   geom_path(data = subset(tromso, id == 8), aes(x, y), lwd = 2)
+#' }
+NULL
